3f5f8ee21221f56096f95aad8775d88dbe283a20
adrianopiacentini <adrianopiacentini03@gmail.com>
2024-12-18 05:38:00 +0100

    Esercizio venerdì

--- react-components/src/App.jsx
+++ react-components/src/App.jsx
@@ -2,18 +2,28 @@ import './App.css'
 import AppHeader from './components/AppHeader.jsx'
 import AppCard from './components/AppCard.jsx'
 import AppFooter from './components/AppFooter.jsx'
+import posts from './data/posts.js'
 
 function App() {
 
   return (
     <>
-        <AppHeader />
-        <div className="container">
-          <AppCard />
-        </div>
-        <AppFooter />
+      <AppHeader />
+      <div className="container">
+        {posts.map((curPost) => {
+          return (
+            curPost.published && (<div className="post" key={curPost.id}>
+              <AppCard id={curPost.id} title={curPost.title} content={curPost.content} image={curPost.image} tags={curPost.tags} />
+            </div>
+            )
+          )
+
+        })}
+      </div>
+      <AppFooter />
     </>
   )
 }
 
 export default App
+
--- react-components/src/components/AppCard.jsx
+++ react-components/src/components/AppCard.jsx
@@ -1,17 +1,22 @@
 import style from './AppCard.module.css';
 import AppButton from './AppButton'
 
-function AppCard() {
+function AppCard({ id, title, image, content, tags }) {
     return (
         <div className={style.card}>
             <div className={style.imgContainer}>
-                <img src={'/images/600x400.png'} />
+                <img src={image ? `/images/${image}` : `/images/600x400.png`} />
             </div>
             <div className={style.textContainer}>
-                <h3>Titolo del Post</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing assumenda, quisquam temporibus! elit. Molestias ipsa, sunt quae atque doloribus optio. Aperiam nemo assumenda, quisquam temporibus! tempore, saepe consequatur assumenda velit quibusdam consectetur atque inventore esse? Assumenda, quisquam temporibus! </p>
+                <h3>{title}</h3>
+                <p>{content}</p>
             </div>
-             <AppButton /> 
+            <div className={style.cardInfo}>
+                <AppButton />
+                {tags.map((curTag, index) => (
+                    <span key={index} className={style[curTag]}>{curTag}</span>
+                ))}
+            </div>
         </div>
     )
 }
